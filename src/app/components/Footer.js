d3ecf382921becde776af4621bc20b24fc924544
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-06-27 12:42:32 -0300

    modifico footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <div className="flex justify-between items-center h-10 px-12 py-10 bg-emerald-800">
-    <div>Copiright</div>
+    <div>Copyright</div>
     <div>Social</div>
   </div>
   )
